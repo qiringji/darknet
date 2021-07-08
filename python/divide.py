a4e5b733628ca6f7502a03e9b047ab37ec57aa41
Yuta Hayakawa <s06166@MacBook-Pro.local>
2021-07-09 07:55:05 +0900

    add current status

--- python/divide.py
+++ python/divide.py
@@ -5,15 +5,15 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
 
-current_dir = '../data/whale_shark'
-g_colab_full_path='/cotent/darknet/data/whale_shark'
+current_dir = '../data/whale_shark_small'
+g_colab_full_path='/content/darknet/data/whale_shark_small'
 
 # Percentage of images to be used for the test set
 percentage_test = 10;
 
 # Create and/or truncate train.txt and test.txt
-file_train = open('whale-shark-train.txt', 'w')  
-file_test = open('whale-shark-test.txt', 'w')# Populate train.txt and test.txt
+file_train = open('whale-shark-small-train.txt', 'w')  
+file_test = open('whale-shark-small-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
